ed84c1a7ce3616915f2af951bb5f7909a48eda5f
lukasz-chase <lukasz.scigaj00@gmail.com>
2025-09-18 14:19:13 +0200

    use env variables

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,26 +1,29 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import { Platform } from "react-native";
 
-export const supabase = createClient(
-  "https://rwvwylunnscchrxmlnhf.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3dnd5bHVubnNjY2hyeG1sbmhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEzNDU5NDAsImV4cCI6MjA0NjkyMTk0MH0.dkuiZKkKepYI_I--bCfZD2Da4UPSseNcTXlO6Dy12Tw",
-  {
-    auth: {
-      storage: {
-        getItem: async (key) => {
-          const value = await AsyncStorage.getItem(key);
-          return value;
-        },
-        setItem: async (key, value) => {
-          await AsyncStorage.setItem(key, value);
-        },
-        removeItem: async (key) => {
-          await AsyncStorage.removeItem(key);
-        },
-      },
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: false,
-    },
-  }
-);
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage:
+      Platform.OS !== "web" || typeof window !== "undefined"
+        ? AsyncStorage
+        : {
+            getItem: async (key) => {
+              const value = await AsyncStorage.getItem(key);
+              return value;
+            },
+            setItem: async (key, value) => {
+              await AsyncStorage.setItem(key, value);
+            },
+            removeItem: async (key) => {
+              await AsyncStorage.removeItem(key);
+            },
+          },
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
